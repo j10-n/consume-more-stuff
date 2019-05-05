b509b832ac7045ebf02c887bf4cde5744e7030c9
Ronson Shiigi <ronsonshiigi@Ronsons-MBP.home>
2019-05-04 15:12:55 -1000

    added delete

--- server/routes/posts/index.js
+++ server/routes/posts/index.js
@@ -1,38 +1,54 @@
 const express = require("express");
 const router = express.Router();
 
-router.route('/')
-.get((req, res) => {
-  return new req.database.Post().fetchAll()
-    .then((posts) => {
-      return res.json(posts);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.sendStatus(500);
-    });
-})
-.post((req, res) => {
-  let image = req.body.image;
-  let title = req.body.title;
-  let description = req.body.description;
-  let genre = req.body.genre;
-  let userId = req.body.userId;
-  let created_at = req.body.created_at;
-  let updated_on = req.body.updated_on;
-
-  return new req.database.Post({
-    image, 
-    title,
-    description,
-    genre,
-    userId,
-    created_at,
-    updated_on
+router
+  .route("/")
+  .get((req, res) => {
+    return new req.database.Post()
+      .fetchAll()
+      .then(posts => {
+        return res.json(posts);
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+      });
   })
-    .save()
-    .then((posts) => {
-      return res.json({ success: true });
+  .post((req, res) => {
+    let image = req.body.image;
+    let title = req.body.title;
+    let description = req.body.description;
+    let genre = req.body.genre;
+    let userId = req.body.userId;
+    let created_at = req.body.created_at;
+    let updated_on = req.body.updated_on;
+
+    return new req.database.Post({
+      image,
+      title,
+      description,
+      genre,
+      userId,
+      created_at,
+      updated_on
+    })
+      .save()
+      .then(posts => {
+        return res.json({ success: true });
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+      });
+  });
+
+router.post("/delete/:id", (req, res) => {
+  let id = req.params.id;
+  console.log(id);
+  return new req.database.Post({ id })
+    .destroy()
+    .then(data => {
+      return res.json(data);
     })
     .catch(err => {
       console.log(err);
