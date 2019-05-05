fa8ebb73a6d63b441bdb0ab59af212aa70c0d427
justen <justen@justens-MBP.home>
2019-05-04 17:52:39 -1000

    template for home and myposts pages:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/home";
+import MyPosts from "./pages/my-posts";
+
+import "./vendor/bootstrap/css/bootstrap.css";
+import "./vendor/bootstrap/css/bootstrap.css.map";
+import "./vendor/bootstrap/css/bootstrap.min.css";
+import "./vendor/bootstrap/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div name="App">
+      {MyPosts()}
+      {/* Footer */}
+      <footer className="py-5 bg-dark">
+        <div className="container">
+          <p className="m-0 text-center text-white">
+            Copyright &copy; Your Website 2019
+          </p>
+        </div>
+        {/* /.container */}
+      </footer>
     </div>
   );
 }
